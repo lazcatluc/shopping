db3091e6783e1d9fff6e89d47970f9652933699d
Catalin Lazar <lazcatluc@gmail.com>
2017-11-27 00:27:59 +0200

    Move tests to corresponding packages

--- src/test/java/ro/contezi/shopping/ShoppingTestConfig.java
+++ src/test/java/ro/contezi/shopping/ShoppingTestConfig.java
@@ -32,13 +32,13 @@ public class ShoppingTestConfig {
                         facebookEntry.getMessaging().forEach(messages::add));
     }
 
-    static String mesageFromUser(String message, String facebookPageUserId) {
+    public static String mesageFromUser(String message, String facebookPageUserId) {
         return "{\"object\":\"page\",\"entry\":[{\"id\":\"535405013470180\",\"time\":1508051848445,\"messaging\":"
                 + "[{\"sender\":{\"id\":\"" + facebookPageUserId + "\"},\"recipient\":{\"id\":\"535405013470180\"},\"timestamp\":1508051847330,\"message\":"
                 + "{\"mid\":\"mid.$cAAHm8o_kpEtlUCpQolfHuUHC30ja\",\"seq\":1171418,\"text\":\"" + message + "\"}}]}]}";
     }
 
-    static String quickReplyFromUser(String payload, String facebookPageUserId) {
+    public static String quickReplyFromUser(String payload, String facebookPageUserId) {
         return "{\"object\":\"page\",\"entry\":[{\"id\":\"535405013470180\",\"time\":1510896979779,\"messaging\":["
                 + "{\"sender\":{\"id\":\"" + facebookPageUserId + "\"},\"recipient\":{\"id\":\"535405013470180\"},\"timestamp\":1510896978831,"
                 + "\"message\":{\"quick_reply\":{\"payload\":\"" + payload + "\"},"
